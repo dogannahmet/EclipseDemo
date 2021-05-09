22b71695bac57e0dfd4af97653ffc058a6a0dead
Ahmet D. <ahmet.msn@live.com>
2021-05-09 13:23:18 +0300

    New Project

--- inheritance2/src/inheritance2/Main.java
+++ inheritance2/src/inheritance2/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		CustomerManager customerManager = new CustomerManager();
 		customerManager.add(new FileLogger());
+		
+		customerManager.add(new SmsLogger());
 
 	}
 
