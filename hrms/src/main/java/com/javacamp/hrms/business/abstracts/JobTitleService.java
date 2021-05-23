ef1b7dadd8bb775486afa1dcd177582058c0450b
Ahmet D. <ahmet.msn@live.com>
2021-05-23 23:10:16 +0300

    Commit 23.05

--- hrms/src/main/java/com/javacamp/hrms/business/abstracts/JobTitleService.java
+++ hrms/src/main/java/com/javacamp/hrms/business/abstracts/JobTitleService.java
@@ -2,8 +2,11 @@ package com.javacamp.hrms.business.abstracts;
 
 import java.util.List;
 
+import com.javacamp.hrms.core.utilities.results.DataResult;
+import com.javacamp.hrms.core.utilities.results.Result;
 import com.javacamp.hrms.entities.concretes.JobTitle;
 
 public interface JobTitleService {
-	List<JobTitle> getAll();
+	DataResult<List<JobTitle>> getAll();
+	Result add(JobTitle jobTitle);
 }
--- hrms/src/main/java/com/javacamp/hrms/business/concretes/JobTitleManager.java
+++ hrms/src/main/java/com/javacamp/hrms/business/concretes/JobTitleManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javacamp.hrms.business.abstracts.JobTitleService;
+import com.javacamp.hrms.core.utilities.results.DataResult;
+import com.javacamp.hrms.core.utilities.results.Result;
+import com.javacamp.hrms.core.utilities.results.SuccessDataResult;
+import com.javacamp.hrms.core.utilities.results.SuccessResult;
 import com.javacamp.hrms.dataAccess.abstracts.JobTitleDao;
 import com.javacamp.hrms.entities.concretes.JobTitle;
 
@@ -24,8 +28,17 @@ public class JobTitleManager implements JobTitleService {
 
 
 	@Override
-	public List<JobTitle> getAll() {
-		return this.jobTitleDao.findAll();
+	public DataResult<List<JobTitle>> getAll() {
+		return new SuccessDataResult<List<JobTitle>>
+		(this.jobTitleDao.findAll(),"Data listelendi.");
+	}
+
+
+
+	@Override
+	public Result add(JobTitle jobTitle) {
+		this.jobTitleDao.save(jobTitle);
+		return new SuccessResult("İş pozisyonu eklendi.");
 	}
 
 }
